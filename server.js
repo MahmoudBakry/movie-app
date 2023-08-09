c0580a31431a7f517e83865a216a3c1fb360840b
Mahmoud Bakrey <mahmoud.ae.bakry@gmail.com>
2023-08-09 22:48:25 +0300

    test server

--- server.js
+++ server.js
@@ -6,7 +6,16 @@ app.get('/',(req, res)=>{
 })
 
 app.get('/hello', (req, res)=>{
-    res.send("Hello now .... I am on the fire...")
+res.status(200)
+res.json({
+    message : "Hello in our movie app", 
+    BE : "Mahmoud Bakry",
+    connect : {
+        email : "mahmoud.ae.bakry@gmail.com", 
+        phone : "01001072579"
+    }
+})
+
 })
 app.listen(3000, ()=>{
     console.log('now i am workink for you,,,,,on port')
