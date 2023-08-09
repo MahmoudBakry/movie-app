dd2dccf1da5682197c673d71dbb8c9a46663c4b9
Mahmoud Bakrey <mahmoud.ae.bakry@gmail.com>
2023-08-09 22:24:01 +0300

    Add new get end point for test

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ let app = express();
 app.get('/',(req, res)=>{
     res.send('Hello in my server')
 })
+
+app.get('/hello', (req, res)=>{
+    res.send("Hello now .... I am on the fire...")
+})
 app.listen(3000, ()=>{
-    console.log('now i am workink for you,,,,,')
+    console.log('now i am workink for you,,,,,on port')
 })
